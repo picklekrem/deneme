d69a6d8b39fdc16fb897e107e2fcbfa117c82c87
Ekrem <ekrem@3pounds.io>
2021-05-12 02:31:54 +0300

    first test, good test

---
+++ denemeGit/denemeGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  denemeGit
+//
+//  Created by Ekrem Özkaraca on 12.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
